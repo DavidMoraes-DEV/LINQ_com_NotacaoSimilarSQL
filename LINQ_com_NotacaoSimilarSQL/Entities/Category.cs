8f66db7a26d79cc4e0df7b6de2dbf2fdd2e490a6
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-11-11 15:37:11 -0300

    LINQ com notacao similar ao SQL

---
+++ LINQ_com_NotacaoSimilarSQL/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_com_NotacaoSimilarSQL.Entities
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
